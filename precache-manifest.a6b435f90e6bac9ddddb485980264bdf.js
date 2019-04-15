26bfbaaa8cfe267dcc57490de00b738090ddb594
ashbshaw <ashley.brooke.shaw@gmail.com>
2019-04-14 21:56:38 -0500

    Updates

---
+++ precache-manifest.a6b435f90e6bac9ddddb485980264bdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f06d0ad94d83b8d00e0a",
+    "url": "/ashbshaw/Clicky-Game/static/js/runtime~main.1d0acdba.js"
+  },
+  {
+    "revision": "c1b333cc7763d4c3054c",
+    "url": "/ashbshaw/Clicky-Game/static/js/main.d0cef110.chunk.js"
+  },
+  {
+    "revision": "a686e41154fd92225c54",
+    "url": "/ashbshaw/Clicky-Game/static/js/2.5bfccd3c.chunk.js"
+  },
+  {
+    "revision": "c1b333cc7763d4c3054c",
+    "url": "/ashbshaw/Clicky-Game/static/css/main.412b881a.chunk.css"
+  },
+  {
+    "revision": "4aa4f034695f34d0012cf3dabfd0f6e8",
+    "url": "/ashbshaw/Clicky-Game/index.html"
+  }
+];
